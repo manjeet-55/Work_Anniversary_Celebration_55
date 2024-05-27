257ce494b4ad1025bd6eb541cc090d46083916ab
AnshulVats55 <anshul.vats@fiftyfivetech.io>
2024-05-27 11:18:53 +0530

    fix: This commit makes this app a PWA by adding manifest.json file and pwa service worker

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -10,14 +9,3 @@ export default defineConfig({
     },
   },
 });
-
-// export default defineConfig({
-//   plugins: [react()],
-//   publicDir: "public",
-//   build: {
-//     outDir: "build",
-//     rollupOptions: {
-//       external: ["react-spring"],
-//     },
-//   },
-// });
